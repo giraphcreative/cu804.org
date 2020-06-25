61a1d55f8698b03b4015b4c5569d5282ed271615
James Pederson <james@jpederson.com>
2020-06-24 19:29:04 -0500

    Add favicon manifest to the header.

--- header.php
+++ header.php
@@ -18,6 +18,11 @@
 <?php wp_head(); ?>
 <link href="<?php bloginfo( "template_url" ) ?>/css/main.css?v=6" rel="stylesheet" type="text/css">
 
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+
 </head>
 <body <?php body_class(); ?>>
 <div class="container">
