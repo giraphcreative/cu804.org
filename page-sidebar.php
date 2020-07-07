14698dfd1f4c74f4ca0a1151be2d0097c7a4cfef
James Pederson <james@jpederson.com>
2020-07-06 20:06:37 -0500

    Set up two column wrappers, and remove font resize at 768px.

--- page-sidebar.php
+++ page-sidebar.php
@@ -11,22 +11,24 @@ the_showcase();
 ?>
 
 <div class="content-wide" role="main">
-	<div class="quarter sidebar">
-		<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-generic') ) : ?>no sidebar selected<?php endif; ?>
-	</div>
-	<div class="three-quarter">
-	<?php 
-	
-	if ( have_posts() ) :
-		while ( have_posts() ) : the_post(); 
-			?>
-	<h1 class="post-title"><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-			<?php
-		endwhile;
-	endif;
+	<div class="wrap">
+		<div class="quarter sidebar">
+			<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('sidebar-generic') ) : ?>no sidebar selected<?php endif; ?>
+		</div>
+		<div class="three-quarter">
+		<?php 
+		
+		if ( have_posts() ) :
+			while ( have_posts() ) : the_post(); 
+				?>
+		<h1 class="post-title"><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+				<?php
+			endwhile;
+		endif;
 
-	?>
+		?>
+		</div>
 	</div>
 </div><!-- #content -->
 
