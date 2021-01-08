2ac4c44480dc14814574896781c044efb7c69641
James Pederson <james@jpederson.com>
2021-01-08 10:56:53 -0600

    Update the calculator library.

--- js/lib/kjecalc/KJESiteSpecific.js
+++ js/lib/kjecalc/KJESiteSpecific.js
@@ -1,26 +1,31 @@
-KJE.sEmailFooter = "\n\n-------------------------------------------------------------\nBC Consulting http://bccstrategies.com\n-------------------------------------------------------------\n"
-// Calculator backgrounds and colors
-KJE.ErrorBackground="#FF7777"; // backgroundColor
-KJE.IncompleteBackground="#FFFF77";
-KJE.ClearColor="#FFFFFF";
-KJE.colorList=["","#dfe0db","#cccccc","#BE4262","#FABB50","#DDCCDD","#CCCCCC","#CCCCDD","#CCDDCC","#CCDDDD","#CCCCDD"];
-
-// Report Header and Footer
-KJE.ReportHeader="<div class='KJEReportTitleBlock'><div class='KJEReportTitle'>**REPORT_TITLE**</div><br><div class=KJELogo><img src='logo.png' class=KJELogo alt='BC Consulting'></div>bccstrategies.com</div>";
-KJE.ReportFooter="<div class=KJECenter><p><span class='KJEBold'>BC Consulting</span><br>220 8 Street SE<br>Washington, DC 20003</p><p class='KJEReportFooter KJECenter'>Information and interactive calculators are made available to you only as self-help tools for your independent use and are not intended to provide investment or tax advice. We cannot and do not guarantee their applicability or accuracy in regards to your individual circumstances. All examples are hypothetical and are for illustrative purposes. We encourage you to seek personalized advice from qualified professionals regarding all personal finance issues.</p></div><!--EXTRA_FOOTER-->";
-
+KJE.sEmailFooter = "\n\n-------------------------------------------------------------\n**COMPANYNAME** http://**URL**\n-------------------------------------------------------------\n"
+//
+// Examples below can be uncommented and changed to modify your calculator deployment
+// IMPORTANT: Do Not Modify the KJE.js file!  The KJE.js file is replaced during the update process
+// All JS customization must be done in this file a calculators specific Parameter file (that end in Params.js.)
+//
+// For information on customizing this file please see:
+// https://www.dinkytown.net/money/V3customize.html
+//
 // Graph fonts, colors and heights
-KJE.gFont           = ["Arial","Arial","Arial"];
-KJE.gFontStyle      = ["bold","bold",""];
-KJE.gFontSize       = [13,10,10];
-KJE.gHeight               = 250;
-KJE.gHeightReport         = 350;
-KJE.gColorBackground      ="#FFFFFF";
-KJE.gColorForeground      ="#000000";
-KJE.gColorGrid            ="#BBBBBB";
-KJE.gColorGridBackground1 ="#FFFFFF";
-KJE.gColorGridBackground2 ="#CCCCCC";
-KJE.gColorAxisLine        ="#666666";
-KJE.gColorText            ="#000000";
-KJE.gColorList            = ["#000055","#246C9D","#64A650","#FFF200","#5f007f","#F15A22","#B72467","#6DC8BF","#FF0000","#ff00ff","#ffff00","#00ffff","#7f007f","#7f0000","#007f7f","#0000ff","#00c8ff","#60ffff","#bfffbf","#ffff90","#a0c8ef"];
+//KJE.gFont           = ["Arial","Arial","Arial"];
+//KJE.gFontStyle      = ["bold","bold",""];
+//KJE.gFontSize       = [13,10,10];
+//KJE.gHeight               = 250;
+//KJE.gHeightReport         = 350;
+//KJE.gColorBackground      ="#FFFFFF";
+//KJE.gColorForeground      ="#000000";
+//KJE.gColorGrid            ="#BBBBBB";
+//KJE.gColorGridBackground1 ="#FFFFFF";
+//KJE.gColorGridBackground2 ="#CCCCCC";
+//KJE.gColorAxisLine        ="#666666";
+//KJE.gColorText            ="#000000";
+
+// Calculator backgrounds and colors
+//KJE.gColorList            = ["#000055","#246C9D","#64A650","#FFF200","#5f007f","#F15A22","#B72467","#6DC8BF","#FF0000","#ff00ff","#ffff00","#00ffff","#7f007f","#7f0000","#007f7f","#0000ff","#00c8ff","#60ffff","#bfffbf","#ffff90","#a0c8ef"];
+//KJE.colorList             =["","#dfe0db","#DDDDDD","#CCCCCC","#CCDDCC","#CCCCDD","#DDCCCC","#CCCCDD","#CCDDCC","#CCDDDD","#CCCCDD"];
+
+// Sample Report Header and Footer Information
+//KJE.ReportHeader="<div class='KJEReportTitleBlock'><div class='KJEReportTitle'>**REPORT_TITLE**</div>cu804.org</div>";
+//KJE.ReportFooter="<div class=KJECenter><p class='KJEReportFooter KJECenter'>Information and interactive calculators are made available to you only as self-help tools for your independent use and are not intended to provide investment or tax advice. We cannot and do not guarantee their applicability or accuracy in regards to your individual circumstances. All examples are hypothetical and are for illustrative purposes. We encourage you to seek personalized advice from qualified professionals regarding all personal finance issues.</p></div><!--EXTRA_FOOTER-->";
 
